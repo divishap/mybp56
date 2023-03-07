01ab29ba9a290b9b9a3093c997b1e482c5553556
divishap <108281118+divishap@users.noreply.github.com>
2023-03-07 15:37:27 +0530

    Add files via upload

---
+++ nature_infor.js
@@ -0,0 +1,19 @@
+function treesRoom() {
+    
+    window.location = "trees_infor.html";
+}
+
+function forestRoom() {
+    
+    window.location = "forest_infor.html";
+}
+
+function grasslandRoom() {
+    
+    window.location = "grassland_infor.html";
+}
+
+function shrubsRoom() {
+    
+    window.location = "shrubs_infor.html";
+}
